39401eac9904960295286b804d61f7bfa5b13b64
Johnny Goth <Jgoth23@gmail.com>
2021-02-13 00:26:16 -0600

    set up pool

---
+++ server/modules/pool.js
@@ -0,0 +1,10 @@
+const pg = require('pg');
+
+// Create a connection to database
+const pool = new pg.Pool({
+  database: 'weekend_to_do_app',
+  host: 'localhost',
+  port: 5432
+});
+
+module.exports = pool;
